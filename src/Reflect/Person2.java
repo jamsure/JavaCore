927a872457b830254f70dcce2d084fb0f76d4c32
fankai <fankai0719@126.com>
2016-11-24 14:07:20 +0800

    sfsdf

---
+++ src/Reflect/Person2.java
@@ -0,0 +1,67 @@
+package Reflect;
+
+interface China1{  
+    public static final String name="Rollen";  
+    public static  int age=20;  
+    public void sayChina();  
+    public void sayHello(String name, int age);  
+}  
+   
+class Person2 implements China{  
+    public Person2() {  
+           
+    }
+    public Person2(String name){  
+        this.name=name;  
+    }  
+    
+    
+    public Person2(int age){  
+        this.age=age;  
+    }  
+    public Person2(String name, int age) {  
+        this.age=age;  
+        this.name=name;  
+    }  
+   /* public Person(String sex,String name){  
+        this.sex=sex;
+        this.name=name;
+    }*/  
+    public String getSex() {  
+        return sex;  
+    }  
+    public void setSex(String sex) {  
+        this.sex = sex;  
+    }  
+    @Override  
+    public void sayChina(){  
+        System.out.println("hello ,china");  
+    }  
+    @Override  
+    public void sayHello(String name, int age){  
+        System.out.println(name+"  "+age);  
+    }
+    @Override  
+    public String toString(){  
+        return "["+this.name+"  "+this.age+"]";  
+    }  
+    private String sex; 
+    private String name;  
+    private int age;  
+}  
+   
+class hello1{  
+    public static void main(String[] args) {  
+        Class<?> demo=null;  
+        try{  
+            demo=Class.forName("Reflect.Person");  
+        }catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        //保存所有的接口  
+        Class<?> intes[]=demo.getInterfaces();  
+        for (int i = 0; i < intes.length; i++) {  
+            System.out.println("实现的接口   "+intes[i].getName());  
+        }  
+    }  
+}  
---
+++ src/Reflect/WWsssss.java
@@ -0,0 +1,10 @@
+package Reflect;
+
+public class WWsssss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
